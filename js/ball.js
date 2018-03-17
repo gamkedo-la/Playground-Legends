e1633ab61a8465da6b51dfe6418f310217083c24
Luth Haroon <luthharoon@gmail.com>
2018-03-17 23:46:16 +0000

    Repositioning of ball & shadow; don't show shadow when ball is held
    Ball & ball shadow are now in line with players' feet

--- js/ball.js
+++ js/ball.js
@@ -2,6 +2,8 @@ const BALL_RADIUS = 20; // must match sprite size
 const BALL_BOUNCE = -0.5; // collision restitution
 const BALL_MOMENTUM = 0.8; // air friction/drag
 const BALL_GRAVITY = 800; // pixels per second
+const BALL_GROUND = FLOOR_Y - 20;
+const SHADOW_GROUND = FLOOR_Y - 15;
 
 var ballSpeedX = 0; // pixels per second
 var ballSpeedY = 0; // pixels per second
@@ -15,34 +17,35 @@ var ballOutOfBoundary; //the boundary is the horizontal screen canvas for now
 
 function drawBall() {
 
-	if (shadowImageLoaded) {
-		canvasContext.drawImage(shadowImage, ballX-BALL_RADIUS, FLOOR_Y+10);
+	if (shadowImageLoaded &&
+		!p1.ballHeld && !p2.ballHeld) {
+		canvasContext.drawImage(shadowImage, ballX - BALL_RADIUS, SHADOW_GROUND);
 	}
 
 	if (ballImageLoaded) {
-		canvasContext.drawImage(ballImage, ballX-BALL_RADIUS, ballY-BALL_RADIUS);
+		canvasContext.drawImage(ballImage, ballX - BALL_RADIUS, ballY - BALL_RADIUS);
 	}
 
 }
 
 function moveBall() {
-    
-		//console.log('moveball pos:'+ballX+','+ballY+' spd:'+ballSpeedX+','+ballSpeedY+' dt:'+secondsSinceLastFrame);
-	
-		//Allows the ball to move anywhere on screen and at a constant speed
-		//regardless of framerate
-		ballX = ballX + (ballSpeedX * secondsSinceLastFrame);
-		ballY = ballY + (ballSpeedY * secondsSinceLastFrame);
-		if(ballY < FLOOR_Y) {
-			ballSpeedY += (BALL_GRAVITY * secondsSinceLastFrame);
-		}
-		else {
-            ballY = FLOOR_Y;
-            ballSpeedX *= BALL_MOMENTUM;
-            ballSpeedY *= BALL_BOUNCE;
-            //ballGroundHandling();
-		}
-    
+
+	//console.log('moveball pos:'+ballX+','+ballY+' spd:'+ballSpeedX+','+ballSpeedY+' dt:'+secondsSinceLastFrame);
+
+	//Allows the ball to move anywhere on screen and at a constant speed
+	//regardless of framerate
+	ballX = ballX + (ballSpeedX * secondsSinceLastFrame);
+	ballY = ballY + (ballSpeedY * secondsSinceLastFrame);
+	if (ballY < BALL_GROUND) {
+		ballSpeedY += (BALL_GRAVITY * secondsSinceLastFrame);
+	}
+	else {
+		ballY = BALL_GROUND;
+		ballSpeedX *= BALL_MOMENTUM;
+		ballSpeedY *= BALL_BOUNCE;
+		//ballGroundHandling();
+	}
+
     /*    function ballGroundHandling() {
                   if(ballSpeedY !== 0 && ballSpeedX !== 0) { //play sound if ball is on ground, but still in motion
                       ballBounced = true;
@@ -51,41 +54,41 @@ function moveBall() {
                 ballBounced = false;
             }
         }*/
-    
-    
-    
-		//This will keep the ball within the frame of the canvas horizontally
-		//also avoid getting stuck in the corner
-		if((ballX < 0 && ballSpeedX < 0.0 ) || (ballX > canvas.width && ballSpeedX > 0.0 )){
-			ballOutOfBoundary = true;
-			ballSpeedX = -ballSpeedX;            
-		}		
-		
-		//This will keep the ball within the frame of the canvas vertically
-		if(ballY > FLOOR_Y || ballY < 0){
-			ballTouchedFloor = true;
-			ballSpeedY = -ballSpeedY;
-		}
+
+
+
+	//This will keep the ball within the frame of the canvas horizontally
+	//also avoid getting stuck in the corner
+	if ((ballX < 0 && ballSpeedX < 0.0) || (ballX > canvas.width && ballSpeedX > 0.0)) {
+		ballOutOfBoundary = true;
+		ballSpeedX = -ballSpeedX;
 	}
-	
+
+	//This will keep the ball within the frame of the canvas vertically
+	if (ballY > FLOOR_Y || ballY < 0) {
+		ballTouchedFloor = true;
+		ballSpeedY = -ballSpeedY;
+	}
+}
+
 function ballCollisionWithPlayers(whichPlayer) {
-	  var diffX = Math.abs(ballX - whichPlayer.x);
-	  var diffY = Math.abs(ballY - whichPlayer.y);
-	  var closeEnough = 50; // measured in pixels
-		if(diffX < closeEnough && diffY < closeEnough 
-			&& ballTouchedFloor == false && ballOutOfBoundary == false) {
-		   ballSpeedX = -ballSpeedX;
-		   ballSpeedY = -ballSpeedY;
-	   }
+	var diffX = Math.abs(ballX - whichPlayer.x);
+	var diffY = Math.abs(ballY - whichPlayer.y);
+	var closeEnough = 50; // measured in pixels
+	if (diffX < closeEnough && diffY < closeEnough
+		&& ballTouchedFloor == false && ballOutOfBoundary == false) {
+		ballSpeedX = -ballSpeedX;
+		ballSpeedY = -ballSpeedY;
+	}
 }
 
 function clamp(val, min, max) {
-    return Math.max(min, Math.min(max, val))
+	return Math.max(min, Math.min(max, val))
 }
 
 // UNTESTED - FIXME - TODO
 // the box must be an AABB (axis-aligned bounding box)
-function circleIsTouchingBox(circleX,circleY,circleRadius,boxX,boxY,boxW,boxH) {
+function circleIsTouchingBox(circleX, circleY, circleRadius, boxX, boxY, boxW, boxH) {
 	// closest point to the circle inside the box
 	var closestX = clamp(circleX, boxX, boxX + boxW);
 	var closestY = clamp(circleY, boxY, boxY + boxH);
