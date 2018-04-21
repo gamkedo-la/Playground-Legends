29ebfe3b5b3ecf821b234e27012f8ccb22a47467
Chris DeLeon <gamkedo@gmail.com>
2018-04-21 00:31:25 -0700

    Credits entered (contributors feel free to update/reword yours)

--- js/credits.js
+++ js/credits.js
@@ -13,6 +13,17 @@ function creditsUpdate() {
     creditsDraw();
 }
 
+function outlineText(words,atX,atY) {
+    var outlineAmt = 2;
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillText(words,atX-outlineAmt,atY);
+    canvasContext.fillText(words,atX+outlineAmt,atY);
+    canvasContext.fillText(words,atX,atY-outlineAmt);
+    canvasContext.fillText(words,atX,atY+outlineAmt);
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillText(words,atX,atY);
+}
+
 function creditsDraw() {
     // Background
     canvasContext.fillStyle = 'black';
@@ -34,6 +45,11 @@ function creditsDraw() {
     //var titleMeasure = canvasContext.measureText(Math.floor(title));
     //canvasContext.fillText(title, (canvas.width / 2) - (titleMeasure.width * 2) - 20, 100);
 
+    // P2
+    canvasContext.globalAlpha = 0.25;
+    canvasContext.drawImage(credits_p2, PLAYERS_X + (PLAYERS_DRAW_WIDTH / 2) - P2_DRAW_OFFSET,PLAYERS_Y, SINGLE_PLAYER_WIDTH,PLAYERS_DRAW_HEIGHT);
+    canvasContext.globalAlpha = 1;
+
     // Sub-Title
     canvasContext.fillStyle = 'black';
     canvasContext.font = 'bold 40px Helvetica';
@@ -41,10 +57,31 @@ function creditsDraw() {
     var subTitleMeasure = canvasContext.measureText(Math.floor(subTitle));
     canvasContext.fillText(subTitle, 350, 150);
 
-    // P2
-    canvasContext.globalAlpha = 0.25;
-    canvasContext.drawImage(credits_p2, PLAYERS_X + (PLAYERS_DRAW_WIDTH / 2) - P2_DRAW_OFFSET,PLAYERS_Y, SINGLE_PLAYER_WIDTH,PLAYERS_DRAW_HEIGHT);
-    canvasContext.globalAlpha = 1;
+    var creditsRows = ["Project Lead, initial functionality, hit counter, drop timeout, sounds (jump, menu, countdown), winner declaration,",
+    "player 1 run animation: Jose Contreras",
+"Jump control, player animations (jump, catching, throwing), sounds (ball hits, catching): Herleen Dualan",
+"Title art and game logo, pause functionality, screenshake, menu decorative programming (overlay, scrolling",
+    "background, faded characters): Dana Alcala",
+"Round end and transition, AI walks to ball, sprite flip: Terrence McDonnell",
+"Scoring bug fix, UI refactor and additional scoreboard code, pause dim, canvas scaling: Randy Tan Shaoxian",
+"Player sprite, music (background, 2 for menu), backgrounds (outdoor sky and mountains): Vignesh Ramesh",
+"Scoring, ducking feature, background switching, round timer, volume control, sound balancing: Phil Nicholson",
+"Ball trail and shadow, mouse aim cursor, ball impact animation, AI tuning: Christer \"McFunkypants\" Kaitila",
+"Classroom background (concept and animation), pause-screen UI blackboard: Charlene A.",
+"Gravity and throw power tuning, AI follows ball in-air, ball collision fix: Naro",
+"AI throws ball, round reset, additional player color: Abdulaziz",
+"Main menu functionality, sprite animation code, player 2 run sprite: Marcelo P. Sequeira",
+"In-game music, trail and UI polish, audio bug fixing: Brandon Trumpold",
+"Time out bug fix, alternate controls: Luth Haroon",
+"Dodge mechanic, AI tactic randomization: Barış Köklü",
+"Catch by click functionality: Christopher McLaughlin"];
+
+    canvasContext.font = '15px Helvetica';
+    var creditsX = 27, creditsY = 175, creditsSkipY = 20;
+    for(var i=0;i<creditsRows.length;i++) {
+        outlineText(creditsRows[i], creditsX, creditsY);
+        creditsY+=creditsSkipY;
+    }
 
     // Back
     canvasContext.fillStyle = 'black';
